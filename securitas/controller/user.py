4a27066572690bba4b60e7af2da0be6c59d30343
Rick Elrod <rick@elrod.me>
2019-11-26 04:21:10 -0500

    Endpoint for user editing but no useful stuff yet
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- securitas/controller/user.py
+++ securitas/controller/user.py
@@ -1,4 +1,4 @@
-from flask import render_template, session
+from flask import flash, redirect, render_template, session, url_for
 
 from securitas import app
 from securitas.utility import with_ipa
@@ -8,3 +8,15 @@ from securitas.utility import with_ipa
 def user(ipa, username):
     user = ipa.user_show(username)
     return render_template('user.html', user=user)
+
+@app.route('/user/<username>/edit/')
+@with_ipa(app, session)
+def user_edit(ipa, username):
+    # TODO: Maybe make this a decorator some day?
+    if session.get('securitas_username') != username:
+        flash(
+            'You do not have permission to edit this account.',
+            'red')
+        return redirect(url_for('user', username=username))
+    user = ipa.user_show(username)
+    return render_template('user-edit.html', user=user)
