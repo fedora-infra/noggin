f056fb7705de7da872b78f1d6915f9590a4c4293
Aurélien Bompard <aurelien@bompard.org>
2020-01-22 16:02:08 +0100

    Test the add and remove member controllers
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- securitas/tests/unit/controller/test_group.py
+++ securitas/tests/unit/controller/test_group.py
@@ -1,6 +1,8 @@
+import mock
 import pytest
 import python_freeipa
 from bs4 import BeautifulSoup
+from flask import get_flashed_messages
 
 from securitas import ipa_admin
 
@@ -24,6 +26,7 @@ def dummy_user_as_group_manager(logged_in_dummy_user, dummy_group):
 def test_groups_list(client, logged_in_dummy_user, dummy_group):
     """Test the groups list: /groups/"""
     result = client.get('/groups/')
+    assert result.status_code == 200
     page = BeautifulSoup(result.data, 'html.parser')
     groups = page.select("ul.collection li[data-group-name='dummy-group']")
     assert len(groups) == 1
@@ -73,3 +76,129 @@ def test_group(client, dummy_user_as_group_manager, make_user):
     # Current user is a sponsor, there must be the corresponding widgets
     assert len(page.select("button[data-target='sponsor-modal']")) == 1
     assert len(page.select("form[action='/group/dummy-group/members/']")) == 1
+
+
+@pytest.mark.vcr()
+def test_group_does_not_exist(client, logged_in_dummy_user):
+    """Return a 404 when the group is not found"""
+    result = client.get('/group/dummy-group/')
+    assert result.status_code == 404
+
+
+@pytest.mark.vcr()
+def test_group_add_member(client, dummy_user_as_group_manager, make_user):
+    """Test adding a member to a group"""
+    make_user("testuser")
+    result = client.post(
+        '/group/dummy-group/members/', data={"new_member_username": "testuser"}
+    )
+    assert result.status_code == 302
+    assert result.location == f"http://localhost/group/dummy-group/"
+    messages = get_flashed_messages(with_categories=True)
+    assert len(messages) == 1
+    category, message = messages[0]
+    assert message == "You got it! testuser has been added to dummy-group."
+    assert category == "green"
+
+
+@pytest.mark.vcr()
+def test_group_add_unknown_member(client, dummy_user_as_group_manager):
+    """Test adding a non-existent member to a group"""
+    result = client.post(
+        '/group/dummy-group/members/', data={"new_member_username": "testuser"}
+    )
+    assert result.status_code == 302
+    assert result.location == f"http://localhost/group/dummy-group/"
+    messages = get_flashed_messages(with_categories=True)
+    assert len(messages) == 1
+    category, message = messages[0]
+    assert message == "User testuser was not found in the system."
+    assert category == "red"
+
+
+@pytest.mark.vcr()
+def test_group_add_member_invalid(client, dummy_user_as_group_manager, make_user):
+    """Test failure when adding a member to a group"""
+    make_user("testuser")
+    with mock.patch("securitas.security.ipa.Client.group_add_member") as method:
+        method.side_effect = python_freeipa.exceptions.ValidationError(
+            message={
+                "member": {"user": [("testuser", "something went wrong")], "group": []}
+            },
+            code="4242",
+        )
+        result = client.post(
+            '/group/dummy-group/members/', data={"new_member_username": "testuser"}
+        )
+    assert result.status_code == 302
+    assert result.location == f"http://localhost/group/dummy-group/"
+    messages = get_flashed_messages(with_categories=True)
+    assert len(messages) == 1
+    category, message = messages[0]
+    assert message == "Unable to add user testuser: something went wrong"
+    assert category == "red"
+
+
+@pytest.mark.vcr()
+def test_group_add_member_invalid_form(client, dummy_user_as_group_manager):
+    """Test an invalid form when adding a member to a group"""
+    result = client.post('/group/dummy-group/members/', data={})
+    assert result.status_code == 302
+    assert result.location == f"http://localhost/group/dummy-group/"
+    messages = get_flashed_messages(with_categories=True)
+    assert len(messages) == 1
+    category, message = messages[0]
+    assert message == "New member username must not be empty"
+    assert category == "red"
+
+
+@pytest.mark.vcr()
+def test_group_remove_member(client, dummy_user_as_group_manager, make_user):
+    """Test removing a member from a group"""
+    make_user("testuser")
+    ipa_admin.group_add_member("dummy-group", users="testuser")
+    result = client.post(
+        '/group/dummy-group/members/remove', data={"username": "testuser"}
+    )
+    assert result.status_code == 302
+    assert result.location == f"http://localhost/group/dummy-group/"
+    messages = get_flashed_messages(with_categories=True)
+    assert len(messages) == 1
+    category, message = messages[0]
+    assert message == "You got it! testuser has been removed from dummy-group."
+    assert category == "green"
+
+
+@pytest.mark.vcr()
+def test_group_remove_member_invalid(client, dummy_user_as_group_manager):
+    """Test failure when removing a member from a group"""
+    with mock.patch("securitas.security.ipa.Client.group_remove_member") as method:
+        method.side_effect = python_freeipa.exceptions.ValidationError(
+            message={
+                "member": {"user": [("testuser", "something went wrong")], "group": []}
+            },
+            code="4242",
+        )
+        result = client.post(
+            '/group/dummy-group/members/remove', data={"username": "testuser"}
+        )
+    assert result.status_code == 302
+    assert result.location == f"http://localhost/group/dummy-group/"
+    messages = get_flashed_messages(with_categories=True)
+    assert len(messages) == 1
+    category, message = messages[0]
+    assert message == "Unable to remove user testuser: something went wrong"
+    assert category == "red"
+
+
+@pytest.mark.vcr()
+def test_group_remove_member_invalid_form(client, dummy_user_as_group_manager):
+    """Test an invalid form when removing a member from a group"""
+    result = client.post('/group/dummy-group/members/remove', data={})
+    assert result.status_code == 302
+    assert result.location == f"http://localhost/group/dummy-group/"
+    messages = get_flashed_messages(with_categories=True)
+    assert len(messages) == 1
+    category, message = messages[0]
+    assert message == "Username must not be empty"
+    assert category == "red"
