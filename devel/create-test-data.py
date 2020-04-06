25574bc87c4d6e113db48c17774f6e1f433427a4
james02135 <james02135@hotmail.com>
2020-04-06 15:23:43 +0100

    added ClientMeta and changed test data

--- devel/create-test-data.py
+++ devel/create-test-data.py
@@ -27,9 +27,9 @@ except python_freeipa.exceptions.FreeIPAError as e:
 
 # create some random users and add them to both the developers and admin groups randomly
 for x in range(50):
-    firstName = fake.first_name()
-    lastName = fake.last_name()
-    username = firstName + str(x)
+    o_givenname = fake.first_name()
+    o_sn = fake.last_name()
+    username = o_givenname + str(x)
     try:
         ipa.user_add(
             username,
@@ -49,13 +49,12 @@ for x in range(50):
             username, new_password=USER_PASSWORD, old_password=USER_PASSWORD,
         )
         if x % 3 == 0:
-            ipa.group_add_member("developers", username, skip_errors=True)
+            ipa.group_add_member("developers", username)
             if x < 10:
                 ipa.group_add_member_manager(
-                    "developers", users=username, skip_errors=True
-                )
+                    "developers", users=username)
         if x % 2 == 0:
-            ipa.group_add_member("admins", username, skip_errors=True)
+            ipa.group_add_member("admins", username)
     except python_freeipa.exceptions.FreeIPAError as e:
         print(e)
 
@@ -67,13 +66,8 @@ try:
         "Test",
         "User",
         "Test User",
-        home_directory=f"/home/fedora/testUser",
-        disabled=False,
-        random_pass=True,
-        fasircnick="testuser",
-        faslocale=None,
-        fastimezone=None,
-        fasgpgkeyid=[],
+        o_homedirectory=f"/home/fedora/testUser",
+        o_random=True,
     )
 except python_freeipa.exceptions.FreeIPAError as e:
     print(e)
