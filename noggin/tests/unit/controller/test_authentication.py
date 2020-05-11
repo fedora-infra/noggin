161bf0d9dc6847db1ebf99ccd4a3348a2b4e91fe
james02135 <james02135@hotmail.com>
2020-05-11 12:03:47 +0100

    noggin #255 fixes
    noggin #255 fixes
    
    changed the description on the test
    
    Signed-off-by: james02135 <james02135@hotmail.com>
    
    changed the description on the test
    
    Signed-off-by: james02135 <james02135@hotmail.com>
    
    noggin #255 fixes
    
    last change
    
    Signed-off-by: james02135 <james02135@hotmail.com>
    
    changed the description on the test
    
    Signed-off-by: james02135 <james02135@hotmail.com>
    
    noggin #255 fixes
    
    last change
    
    Signed-off-by: james02135 <james02135@hotmail.com>
    
    noggin #255 fixes
    
    changed the description on the test
    
    Signed-off-by: james02135 <james02135@hotmail.com>
    
    changed the description on the test
    
    Signed-off-by: james02135 <james02135@hotmail.com>
    
    noggin #255 fixes
    
    last change
    
    Signed-off-by: james02135 <james02135@hotmail.com>
    
    noggin #255 fixes
    
    Signed-off-by: james02135 <james02135@hotmail.com>
    
    changed the description on the test
    
    Signed-off-by: james02135 <james02135@hotmail.com>
    
    noggin #255 fixes
    
    last change
    
    Signed-off-by: james02135 <james02135@hotmail.com>

--- noggin/tests/unit/controller/test_authentication.py
+++ noggin/tests/unit/controller/test_authentication.py
@@ -109,6 +109,44 @@ def test_login_no_username(client):
     assert "noggin_username" not in session
 
 
+def test_login_username_case(client, dummy_user):
+    """Test giving username random uppercase letter"""
+    result = client.post(
+        '/',
+        data={
+            "login-username": "duMmy",
+            "login-password": "dummy_password",
+            "login-submit": "1",
+        },
+        follow_redirects=True,
+    )
+    page = BeautifulSoup(result.data, 'html.parser')
+    messages = page.select(".flash-messages .alert-success")
+    assert len(messages) == 1
+    assert messages[0].get_text(strip=True) == 'Welcome, dummy!×'
+    assert session.get("noggin_username") == "dummy"
+    assert session.get("noggin_session") is not None
+    
+
+def test_login_username_created_with_case(client, dummy_user_with_case):
+    """Test changing username to lowercase when created with uppercase"""
+    result = client.post(
+        '/',
+        data={
+            "login-username": "dummy",
+            "login-password": "duMmy_password",
+            "login-submit": "1",
+        },
+        follow_redirects=True,
+    )
+    page = BeautifulSoup(result.data, 'html.parser')
+    messages = page.select(".flash-messages .alert-success")
+    assert len(messages) == 1
+    assert messages[0].get_text(strip=True) == 'Welcome, dummy!×'
+    assert session.get("noggin_username") == "dummy"
+    assert session.get("noggin_session") is not None
+
+
 @pytest.mark.vcr()
 def test_login_incorrect_password(client, dummy_user):
     """Test a incorrect password"""
