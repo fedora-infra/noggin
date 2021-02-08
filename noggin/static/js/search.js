9c7ce0efe913a805b5f6a6ae84abe7a83623313d
Ryan Lerch <rlerch@redhat.com>
2021-02-08 19:07:43 +1000

    Update search with loading animations
    Previously, there were no loading animations for the search box, and due
    to the fact we are doing two seperate calls (for users and groups),
    these could return at different times. So this commit adds sepearte
    spinner animations for the two results classes.
    
    Additionally, this moves the string for "users" and "groups" that is
    used to label the groups in the search dropdown into the templates as
    variables, so they can be translated.
    
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- noggin/static/js/search.js
+++ noggin/static/js/search.js
@@ -24,14 +24,18 @@ $("#search")
       display: "uid",
       source: bloodhound_users,
       templates: {
-        header: "<h5>Users</h5>",
+        header: "<h5 class='mb-0'>"+USERS_SEARCH_LABEL+"</h5>",
+        pending: "<h5 class='mb-0'>"+USERS_SEARCH_LABEL+"</h5><h5 class='text-center'><i class='fa fa-circle-o-notch fa-spin fa-fw'></i></h5>"
       },
     },
     {
       name: "groups",
       display: "cn",
       source: bloodhound_groups,
-      templates: { header: "<h5>Groups</h5>" },
+      templates: { 
+        header: "<h5 class='mb-0'>"+GROUPS_SEARCH_LABEL+"</h5>",
+        pending: "<h5 class='mb-0'>"+GROUPS_SEARCH_LABEL+"</h5><h5 class='text-center'><i class='fa fa-circle-o-notch fa-spin fa-fw'></i></h5>"
+      },
     }
   )
   .on("typeahead:selected", function (evt, itm, name) {
