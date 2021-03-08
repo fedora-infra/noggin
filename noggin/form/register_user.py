fedora-infra/noggin

commit 47b8b42ffb68f3a954c31df9c840f44ad8ab640e

Forbid users under 16 to register an account
Fixes: #518
Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

noggin/form/register_user.py
@@ -1,5 +1,5 @@
 from flask_babel import lazy_gettext as _
-from wtforms import PasswordField, StringField
+from wtforms import BooleanField, PasswordField, StringField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, EqualTo
 
@@ -34,6 +34,15 @@ class RegisterUserForm(ModestForm):
             Email(message=_('Email must be valid')),
         ],
         filters=[strip],
+    )
+
+    underage = BooleanField(
+        _('I am over 16 years old'),
+        validators=[
+            DataRequired(
+                message=_("You must be over 16 years old to create an account")
+            )
+        ],
     )
 
     submit = SubmitButtonField(_("Register"))

noggin/tests/unit/controller/test_registration.py
@@ -34,19 +34,17 @@ def post_data_step_1():
         "register-lastname": "User",
         "register-mail": "dummy@example.com",
         "register-username": "dummy",
+        "register-underage": "true",
         "register-submit": "1",
     }
 
 
 @pytest.fixture
-def post_data_non_ascii():
-    return {
-        "register-firstname": "习近平 äöü ß",
-        "register-lastname": "ÄÖÜ ẞ 安倍 晋三",
-        "register-mail": "dummy@example.com",
-        "register-username": "dummy",
-        "register-submit": "1",
-    }
+def post_data_non_ascii(post_data_step_1):
+    post_data_step_1.update(
+        {"register-firstname": "习近平 äöü ß", "register-lastname": "ÄÖÜ ẞ 安倍 晋三"}
+    )
+    return post_data_step_1
 
 
 @pytest.fixture
@@ -554,6 +552,18 @@ def test_empty_email(client, post_data_step_1):
     result = client.post('/', data=post_data_step_1)
     assert_form_field_error(
         result, field_name="register-mail", expected_message='Email must not be empty'
+    )
+
+
+@pytest.mark.vcr()
+def test_underage(client, post_data_step_1):
+    """Register a user that is too young"""
+    post_data_step_1["register-underage"] = ""
+    result = client.post('/', data=post_data_step_1)
+    assert_form_field_error(
+        result,
+        field_name="register-underage",
+        expected_message="You must be over 16 years old to create an account",
     )
 
 

noggin/templates/_register_form.html
@@ -12,6 +12,9 @@
     <div class="form-row">
       <div class="form-group col">{{ macros.with_errors(register_form.mail, tabindex="7") }}</div>
     </div>
+    <div class="form-row form-check">
+      <div class="form-group col">{{ macros.with_errors(register_form.underage, tabindex="8") }}</div>
+    </div>
   </div>
   <div class="card-footer d-flex justify-content-between">
     <div>{{ macros.non_field_errors(register_form) }}</div>

noggin/tests/unit/controller/cassettes/test_registration/test_underage.yaml
@@ -1,0 +1,70 @@
+interactions:
+- request:
+    body: user=admin&password=adminPassw0rd%21
+    headers:
+      Accept:
+      - text/plain
+      Accept-Encoding:
+      - gzip, deflate
+      Connection:
+      - keep-alive
+      Content-Length:
+      - '36'
+      Content-Type:
+      - application/x-www-form-urlencoded
+      Referer:
+      - https://ipa.noggin.test/ipa/session/login_password
+      User-Agent:
+      - python-requests/2.25.1
+    method: POST
+    uri: https://ipa.noggin.test/ipa/session/login_password
+  response:
+    body:
+      string: '<html>
+
+        <head>
+
+        <title>401 Unauthorized</title>
+
+        </head>
+
+        <body>
+
+        <h1>Invalid Authentication</h1>
+
+        <p>
+
+        <strong>kinit: Password incorrect while getting initial credentials
+
+        </strong>
+
+        </p>
+
+        </body>
+
+        </html>'
+    headers:
+      Cache-Control:
+      - no-cache, private
+      Connection:
+      - Keep-Alive
+      Content-Security-Policy:
+      - frame-ancestors 'none'
+      Content-Type:
+      - text/html; charset=utf-8
+      Date:
+      - Thu, 04 Mar 2021 11:27:49 GMT
+      Keep-Alive:
+      - timeout=30, max=100
+      Server:
+      - Apache/2.4.46 (Fedora) OpenSSL/1.1.1g mod_wsgi/4.7.1 Python/3.9 mod_auth_gssapi/1.6.3
+      Transfer-Encoding:
+      - chunked
+      X-Frame-Options:
+      - DENY
+      X-IPA-Rejection-Reason:
+      - invalid-password
+    status:
+      code: 401
+      message: Unauthorized
+version: 1

commit bd1a1cf61b320dba0dc18dd27194b7cf8ae4c4d3

Lower minimum age for registration to 13 to match FAS
The United States Children's Online Privacy Protection Act (U.S. COPPA)
states the minimum age is 13. Changing this number has other undesirable
consequences, so we should not unless there's an actual reason to do so.

Fixes: 47b8b42ffb68f3a954c31df9c840f44ad8ab640e
Fixes: #518

Signed-off-by: Neal Gompa <ngompa13@gmail.com>

noggin/form/register_user.py
@@ -37,10 +37,10 @@ class RegisterUserForm(ModestForm):
     )
 
     underage = BooleanField(
-        _('I am over 16 years old'),
+        _('I am over 13 years old'),
         validators=[
             DataRequired(
-                message=_("You must be over 16 years old to create an account")
+                message=_("You must be over 13 years old to create an account")
             )
         ],
     )

noggin/tests/unit/controller/test_registration.py
@@ -563,7 +563,7 @@ def test_underage(client, post_data_step_1):
     assert_form_field_error(
         result,
         field_name="register-underage",
-        expected_message="You must be over 16 years old to create an account",
+        expected_message="You must be over 13 years old to create an account",
     )
 
 