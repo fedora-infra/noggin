42388d0b95d4d57a7a8fc502c4c1ed2d45fe68d1
Ryan Lerch <rlerch@redhat.com>
2021-03-17 22:12:17 +1000

    make pagination a little neater for large pages
    Implements the pagination layout that shows the start page, end page,
    and a range of pages in between depending on a margin given.
    
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- noggin/tests/unit/utility/test_pagination.py
+++ noggin/tests/unit/utility/test_pagination.py
@@ -19,7 +19,7 @@ def many_dummy_groups(ipa_testing_config):
             ]
         )
 
-    group_list = [f"dummy-group-{i:02d}" for i in range(1, 11)]
+    group_list = [f"dummy-group-{i:02d}" for i in range(1, 200)]
     ipa_admin.batch(
         a_methods=[
             {"method": "group_add", "params": [[name], {"fasgroup": True}]}
@@ -75,8 +75,104 @@ def test_groups_page(client, logged_in_dummy_user, many_dummy_groups):
     next_link = pagination_bar.select_one("li:last-child a.page-link")
     assert next_link is not None
     assert next_link["href"] == "/groups/?page_number=3&page_size=3"
-    # Other links
-    assert len(pagination_bar.select("li.page-item")) == 6
+
+    page_items = pagination_bar.select("li.page-item")
+    pages_bar_list = [
+        p.select(".page-link:last-child")[0].get_text(strip=True) for p in page_items
+    ]
+    assert len(pages_bar_list) == 17
+
+
+@pytest.mark.parametrize(
+    "page_number,page_size,page_result",
+    [
+        (
+            "2",
+            "3",
+            [
+                'Previous',
+                '1',
+                '2(current)',
+                '3',
+                '4',
+                '5',
+                '6',
+                '7',
+                '8',
+                '9',
+                '10',
+                '11',
+                '12',
+                '13',
+                '...',
+                '67',
+                'Next',
+            ],
+        ),
+        (
+            "67",
+            "3",
+            [
+                'Previous',
+                '1',
+                '...',
+                '55',
+                '56',
+                '57',
+                '58',
+                '59',
+                '60',
+                '61',
+                '62',
+                '63',
+                '64',
+                '65',
+                '66',
+                '67(current)',
+            ],
+        ),
+        (
+            "30",
+            "3",
+            [
+                'Previous',
+                '1',
+                '...',
+                '24',
+                '25',
+                '26',
+                '27',
+                '28',
+                '29',
+                '30(current)',
+                '31',
+                '32',
+                '33',
+                '34',
+                '35',
+                '36',
+                '...',
+                '67',
+                'Next',
+            ],
+        ),
+        ("1", "40", ['1(current)', '2', '3', '4', '5', 'Next']),
+    ],
+)
+@pytest.mark.vcr()
+def test_pagination_bar(
+    client, logged_in_dummy_user, many_dummy_groups, page_number, page_size, page_result
+):
+    result = client.get(f"/groups/?page_number={page_number}&page_size={page_size}")
+    assert result.status_code == 200
+    page = BeautifulSoup(result.data, 'html.parser')
+    pagination_bar = page.select_one("ul.pagination")
+    page_items = pagination_bar.select("li.page-item")
+    pages_bar_list = [
+        p.select(".page-link:last-child")[0].get_text(strip=True) for p in page_items
+    ]
+    print(pages_bar_list)
+    assert pages_bar_list == page_result
 
 
 @pytest.mark.vcr()
@@ -131,6 +227,6 @@ def test_mounted_subdir(client, logged_in_dummy_user, many_dummy_groups):
     page = BeautifulSoup(result.data, 'html.parser')
     # Check all links
     links = page.select("ul.pagination a.page-link")
-    assert len(links) == 5
+    assert len(links) == 15
     for link in links:
         assert link["href"].startswith("/subdir/groups/?page_number=")
