5d09c0e33b67b0c6858ff7f104a33c0296c27289
Aurélien Bompard <aurelien@bompard.org>
2021-05-03 13:16:20 +0200

    Display the version in the footer
    Fixes: #592
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- noggin/controller/__init__.py
+++ noggin/controller/__init__.py
@@ -1,5 +1,8 @@
+import os
+
 from flask import Blueprint, g, render_template, session
 
+from noggin import __version__
 from noggin.utility.templates import gravatar
 
 
@@ -13,9 +16,21 @@ def page_not_found(e):
 
 @blueprint.app_context_processor
 def inject_global_template_vars():
+    version = __version__
+    if (
+        "OPENSHIFT_BUILD_COMMIT" in os.environ
+        and "OPENSHIFT_BUILD_REFERENCE" in os.environ
+    ):
+        version_ext = [
+            os.environ['OPENSHIFT_BUILD_REFERENCE'],
+            os.environ['OPENSHIFT_BUILD_COMMIT'][:7],
+        ]
+        version = f"{version} ({':'.join(version_ext)})"
+
     return dict(
         gravatar=gravatar,
         ipa=g.ipa if 'ipa' in g else None,
         current_user=g.current_user if 'current_user' in g else None,
         current_username=session.get('noggin_username'),
+        noggin_version=version,
     )
