5c0988d6383c54ed1427a41daa7037ae9e244661
Aurélien Bompard <aurelien@bompard.org>
2021-06-06 09:33:56 +0200

    Use a script to run liccheck
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

---
+++ devel/run-liccheck.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+trap 'rm -f "$TMPFILE"' EXIT
+
+set -e
+
+TMPFILE=$(mktemp -t noggin-requirements-XXXXXX.txt)
+
+# Note: we can't use poetry export because it isn't smart enough with conditional dependencies:
+# flake8 requires importlib_metadata on python < 3.8, so it's not installed, but it's exported
+# and liccheck crashes on packages listed in the req file but not installed.
+# poetry export --dev -f requirements.txt -o $TMPFILE
+
+pip freeze --exclude-editable --isolated > $TMPFILE
+liccheck -r $TMPFILE
