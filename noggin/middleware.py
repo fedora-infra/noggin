6fd1d74c41dbee2dc9751a9f596b0dd4f5e4b41e
Aurélien Bompard <aurelien@bompard.org>
2023-01-16 12:27:24 +0100

    Fix compatibility with Flask 2.2.X
    The WSGI error handler no longer has access to the request context, so
    we can't use a plain WSGI application to handle IPA errors.
    
    Use Flask's `register_error_handler` instead.
    
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- noggin/middleware.py
+++ noggin/middleware.py
@@ -9,20 +9,10 @@ class IPAErrorHandler:
             self.init_app(app)
 
     def init_app(self, app):
-        app.wsgi_app = IPAWSGIMiddleware(app.wsgi_app, self.get_error_response)
+        app.register_error_handler(
+            python_freeipa.exceptions.FreeIPAError, self.get_error_response
+        )
 
     def get_error_response(self, error):
         current_app.logger.error(f"Uncaught IPA exception: {error}")
         return make_response(render_template(self.template, error=error), 500)
-
-
-class IPAWSGIMiddleware:
-    def __init__(self, wsgi_app, error_factory):
-        self.wsgi_app = wsgi_app
-        self.error_factory = error_factory
-
-    def __call__(self, environ, start_response):
-        try:
-            return self.wsgi_app(environ, start_response)
-        except python_freeipa.exceptions.FreeIPAError as e:
-            return self.error_factory(e)(environ, start_response)
