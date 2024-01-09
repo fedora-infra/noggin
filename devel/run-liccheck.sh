ea2edfbc757527d0a11c1d863cd7b232de6f9e3e
Aurélien Bompard <aurelien@bompard.org>
2024-01-09 10:30:40 +0100

    Update the liccheck script to use the shared licenses list
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- devel/run-liccheck.sh
+++ devel/run-liccheck.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
+#
+# SPDX-FileCopyrightText: Contributors to the Fedora Project
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
-trap 'rm -f "$TMPFILE"' EXIT
+
+STRATEGY_URL=https://raw.githubusercontent.com/fedora-infra/shared/main/liccheck-strategy.ini
+
+trap 'rm -f "$TMPFILE $STRATEGY_TMPFILE"' EXIT
 
 set -e
+set -x
 
-TMPFILE=$(mktemp -t noggin-requirements-XXXXXX.txt)
+TMPFILE=$(mktemp -t requirements-XXXXXX.txt)
+STRATEGY_TMPFILE=$(mktemp -t liccheck-strategy-XXXXXX.ini)
 
-pip install --upgrade "poetry>=1.2"
-poetry install
-poetry export --dev -f requirements.txt -o $TMPFILE
-poetry run liccheck -r $TMPFILE
+curl -o $STRATEGY_TMPFILE $STRATEGY_URL
+
+poetry export --with dev --without-hashes -f requirements.txt -o $TMPFILE
+
+# Use pip freeze instead of poetry when it fails
+# poetry run pip freeze --exclude-editable --isolated > $TMPFILE
+
+poetry run liccheck -r $TMPFILE -s $STRATEGY_TMPFILE
