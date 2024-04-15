87f1ee28a9f6497598f9ae8c2b335d17fd399d1c
Aurélien Bompard <aurelien@bompard.org>
2024-04-15 14:27:21 +0200

    Workaround for the licenses check
    There's an issue with the poetry dependency exporter when two different
    versions of a dependency are in the selected set:
    https://github.com/python-poetry/poetry-plugin-export/issues/183
    
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- devel/run-liccheck.sh
+++ devel/run-liccheck.sh
@@ -7,17 +7,23 @@
 
 STRATEGY_URL=https://raw.githubusercontent.com/fedora-infra/shared/main/liccheck-strategy.ini
 
-trap 'rm -f "$TMPFILE $STRATEGY_TMPFILE"' EXIT
+trap 'rm -rf "$TMPFILE $STRATEGY_TMPFILE $LOCK_TMPDIR"' EXIT
 
 set -e
 set -x
 
 TMPFILE=$(mktemp -t requirements-XXXXXX.txt)
 STRATEGY_TMPFILE=$(mktemp -t liccheck-strategy-XXXXXX.ini)
+LOCK_TMPDIR=$(mktemp -d -t poetry-lock-XXXXXX)
+
+mkdir -p $LOCK_TMPDIR
+# Workaround https://github.com/python-poetry/poetry-plugin-export/issues/183
+sed -e '/^urllib3 = {version = "<2"/d' poetry.lock > $LOCK_TMPDIR/poetry.lock
+cp -p pyproject.toml $LOCK_TMPDIR/
 
 curl -o $STRATEGY_TMPFILE $STRATEGY_URL
 
-poetry export --with dev --without-hashes -f requirements.txt -o $TMPFILE
+poetry export --with dev --without-hashes -f requirements.txt -o $TMPFILE -C $LOCK_TMPDIR
 
 # Use pip freeze instead of poetry when it fails
 # poetry run pip freeze --exclude-editable --isolated > $TMPFILE
