a58b7a80b8fb6f7da6ed24ecba129247280d077c
jkhall81 <jason.kei.hall@gmail.com>
2025-12-10 06:28:44 -0700

    Finalized FPCA warning config, updated defaults and devel settings, ensured compatibility with user settings agreements.
    Signed-off-by: jkhall81 <jason.kei.hall@gmail.com>

--- noggin/defaults.py
+++ noggin/defaults.py
@@ -67,7 +67,7 @@ FEDORA_MESSAGING_ENABLED = False
 # Agreement warnings shown when the corresponding agreement is not signed.
 # Keys must match the agreement.name from FreeIPA.
 AGREEMENT_WARNINGS = {
-    "FPCA": _(
+    "Fedora Project Contributor Agreement": _(
         "Not signing the FPCA will prevent you from logging in to many Fedora "
         "services such as Pagure, Fedora Discussion, and other contributor "
         "platforms."
